c93a51f59b15dcf40602616f962b7dce4f29de82
Rafael <velofuri@gmail.com>
2022-12-11 09:00:04 -0300

    EndPonit para resumo das transacoes

--- financas/views.py
+++ financas/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets, filters, generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from financas.models import Receita, Despesa
 from financas.serializers import ReceitaSerializer, DespesaSerializer
 from django_filters.rest_framework import DjangoFilterBackend
@@ -34,3 +36,51 @@ class DespesasPorMes(generics.ListAPIView):
         queryset = Despesa.objects.filter(data__year=ano, data__month=mes)
         return queryset
     serializer_class = DespesaSerializer
+
+class ResumoPorMes(APIView):
+    def get(self, request, ano, mes):
+        ano = self.kwargs['ano']
+        mes = self.kwargs['mes']
+        total_receitas = self.total_receitas(ano, mes)
+        total_despesas = self.total_despesas(ano, mes)
+        saldo = total_receitas - total_despesas
+        total_gasto_por_categoria = self.total_gasto_por_categoria(ano, mes)
+        return Response(data={
+            'total_receitas': total_receitas,
+            'total_despesas': total_despesas,
+            'saldo': saldo,
+            'total_gasto_por_categoria': total_gasto_por_categoria
+        })
+
+    def total_receitas(self, ano, mes):
+        receitas = Receita.objects.filter(data__year=ano, data__month=mes)
+        valor = 0
+        for receita in receitas:
+            valor += receita.valor
+        return valor
+
+    def total_despesas(self, ano, mes):
+        despesas = Despesa.objects.filter(data__year=ano, data__month=mes)
+        valor = 0
+        for despesa in despesas:
+            valor += despesa.valor
+        return valor
+
+    def total_gasto_por_categoria(self, ano, mes):
+        despesas = Despesa.objects.filter(data__year=ano, data__month=mes)
+        categorias = self.get_dict_categorias()
+        for item in despesas:
+            categorias[item.categoria] += item.valor
+        return categorias
+    
+    def get_dict_categorias(self):
+        return {
+            "alimentacao": 0,
+            "saude": 0,
+            "moradia": 0,
+            "transporte": 0,
+            "educacao": 0,
+            "lazer": 0,
+            "imprevisto": 0,
+            "outras": 0
+        }
--- setup/urls.py
+++ setup/urls.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
-from financas.views import ReceitaViewSet, DespesaViewSet, ReceitasPorMes, DespesasPorMes
+from financas.views import ReceitaViewSet, DespesaViewSet, ReceitasPorMes, DespesasPorMes, ResumoPorMes
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('receita', ReceitaViewSet, basename='Receitas')
-router.register('despesa', DespesaViewSet, basename='Despesas')
+router.register('receitas', ReceitaViewSet, basename='Receitas')
+router.register('despesas', DespesaViewSet, basename='Despesas')
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include(router.urls)),
     path('receita/<int:ano>/<int:mes>', ReceitasPorMes.as_view()),
-    path('despesa/<int:ano>/<int:mes>', DespesasPorMes.as_view())
+    path('despesa/<int:ano>/<int:mes>', DespesasPorMes.as_view()),
+    path('resumo/<int:ano>/<int:mes>', ResumoPorMes.as_view())
 ]
